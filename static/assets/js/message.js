c67eb7ff23c3e7bbecaa1e1ede033f34bca4f717
Bicky Yadav <yadavbicky99@gmail.com>
2024-04-18 03:40:22 +0545

    chat problems fixed

--- static/assets/js/message.js
+++ static/assets/js/message.js
@@ -50,57 +50,59 @@ socket.onclose = async function(e){
     console.log('close', e)
 }
 
+
 function newMessage(message, sent_by_id, thread_id) {
-    if ($.trim(message) === '') {
-        return false;
+	if ($.trim(message) === '') {
+		return false;
+	}
+	let message_element;
+	let chat_id = 'chat_' + thread_id
+	if(sent_by_id == USER_ID){
+	    message_element = `
+			<div class="d-flex mb-4 replied">
+				<div class="msg_cotainer_send">
+					${message}
+					<span class="msg_time_send">8:55 AM, Today</span>
+				</div>
+				<div class="img_cont_msg">
+					<img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" class="rounded-circle user_img_msg">
+				</div>
+			</div>
+	    `
     }
-    let message_element;
-    let chat_id = 'chat_' + thread_id
-    if (sent_by_id == USER_ID) {
-        message_element = `
-        <div class="d-flex mb-4 replied">
-        <div class="img_cont_msg">
-            <img src="https://www.bootdey.com/img/Content/avatar/avatar3.png" class="rounded-circle" style="width: 40px; height: 40px;">
-        </div>
-        <div class="msg_cotainer_send">
-            ${message}
-            <span class="msg_time">8:40 AM, Today</span>
-        </div>
-    </div>
-    
+	else{
+	    message_element = `
+           <div class="d-flex mb-4 received">
+              <div class="img_cont_msg">
+                 <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" class="rounded-circle user_img_msg">
+              </div>
+              <div class="msg_cotainer">
+                 ${message}
+              <span class="msg_time">8:40 AM, Today</span>
+              </div>
+           </div>
         `
-    } else {
-        message_element = `
-        <div class="d-flex mb-4 received">
-            <div class="img_cont_msg">
-                <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle" style="width: 40px; height: 40px;">
-            </div>
-            <div class="msg_cotainer" style="width: calc(100% - 50px);"> <!-- Adjust width to accommodate image width and spacing -->
-                ${message}
-                <span class="msg_time">8:40 AM, Today</span>
-            </div>
-        </div>
-    
-        `
+
     }
 
     let message_body = $('.messages-wrapper[chat-id="' + chat_id + '"] .msg_card_body')
-    message_body.append($(message_element))
+	message_body.append($(message_element))
     message_body.animate({
         scrollTop: $(document).height()
     }, 100);
-    input_message.val(null);
+	input_message.val(null);
 }
 
 
-$('.contact-li').on('click', function () {
+$('.contact-li').on('click', function (){
     $('.contacts .active').removeClass('active')
     $(this).addClass('active')
 
     // message wrappers
     let chat_id = $(this).attr('chat-id')
-    $('.messages-wrapper.is_active').removeClass('is_active').addClass('hide')
-    $('.messages-wrapper[chat-id="' + chat_id + '"]').removeClass('hide').addClass('is_active')
+    $('.messages-wrapper.is_active').removeClass('is_active')
+    $('.messages-wrapper[chat-id="' + chat_id +'"]').addClass('is_active')
+
 })
 
 function get_active_other_user_id(){
